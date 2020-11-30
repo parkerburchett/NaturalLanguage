3bff5c9d0343f13ff5a81df4a1cd5bc99ff371b7
Parker Burchett <parkerburchett@gmail.com>
2020-11-30 13:11:43 -0800

    Testing of sentiment(text)
    right now everything is 100% confident that the string is negative.

--- OwnPrograms/testSeniment.py
+++ OwnPrograms/testSeniment.py
@@ -1,4 +1,19 @@
 import NaturalLanguage.LectureNotes.SentimentAnalysisModule as s
 
 
-s.sentiment("The movie was bad and awful")
+print(s.sentiment("The movie was bad and awful"))
+
+
+print(s.sentiment("I found the movie trite and plot development lacking"))
+
+
+print(s.sentiment("The cinematography was breathtaking in its ability to capture the mood of the era"))
+
+
+
+print(s.sentiment("it's too bad my schlong is this long otheriwise i'd be banging bitch's all day long"))
+
+print(s.sentiment("it made me laugh"))
+
+
+print(s.sentiment("I loved the movie and was happy I saw it"))
