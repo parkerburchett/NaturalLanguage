84dbaebbcaf8c1f9332396e85995d8ef807e4ad2
Parker Burchett <parkerburchett@gmail.com>
2020-12-19 18:41:34 -0800

    Added more printed updates

--- OwnPrograms/Train_On_Kaggle.py
+++ OwnPrograms/Train_On_Kaggle.py
@@ -1,30 +1,54 @@
-from nltk import SklearnClassifier
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVR
 from NaturalLanguage.custom_NLTK_Utils import dataLabeling as dl
 from NaturalLanguage.custom_NLTK_Utils import AlgoEvalutationUtils as AE
+from NaturalLanguage.custom_NLTK_Utils import Inital_Pickle as ip
 import nltk
 import datetime
+import random
 
+
+# source: https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVR.html
 def train_on_kaggle_data():
     start = datetime.datetime.now()
     print('started')
     inputFile = open(r"C:\Users\parke\Documents\GitHub\NaturalLanguage\NaturalLanguage\Datasets\LabeledTweets.csv", "r")
     documents = dl.assemble_kaggle_documents(inputFile)
+    print('Created documents :{}'.format(str(datetime.datetime.now() - start)))
+    all_words = []
+    for d in documents:
+        all_words.append(d[0].lower())
+    all_wordsFRQ = nltk.FreqDist(all_words)
+    print('Created all_words:{}'.format(str(datetime.datetime.now() - start)))
 
-    print('Created Documents :{}'.format(str(datetime.datetime.now() - start)))
-    feature_sets = dl.kaggle_create_feature_sets(documents,5000)
-    
-    print('Created Feature_sets :{}'.format(str(datetime.datetime.now() - start)))
+
+    dict(sorted(all_wordsFRQ.items(), key=lambda item: item[1]))
+    word_features = list(all_words)[:5000]
+    print('Created word_features :{}'.format(str(datetime.datetime.now() - start)))
+
+    # very Hight Timecost. You might just want to pickle this after it runs.
+    feature_sets = [(dl.find_Features(text, word_features), category)
+                    for (text, category) in documents]
+
+    ip.customPickle(feature_sets, "KaggleN5000_feature_sets_preShuffle")
+    random.shuffle(feature_sets)
+
+    print('Created feature_sets :{}'.format(str(datetime.datetime.now() - start)))
 
     train_set = AE.getTrainData(feature_sets)
-    LinearSVCClassifier = SklearnClassifier(LinearSVC())
-    LinearSVCClassifier.train(train_set)
+    LinearSupportVectorRegression = nltk.SklearnClassifier(LinearSVR())
+    LinearSupportVectorRegression.train(train_set)
 
-    print('Trained Linear SCV :{}'.format(str(datetime.datetime.now() - start)))
+    print('Trained Linear SVR :{}'.format(str(datetime.datetime.now() - start)))
     test_data =AE.getTestData(feature_sets)
-    print('Accuracy: {}'.format(str(nltk.classify.accuracy(LinearSVCClassifier, test_data) * 100)))
+
+    print('Accuracy: {}'.format(str(nltk.classify.accuracy(LinearSupportVectorRegression, test_data) * 100)))
+
+    ip.customPickle(LinearSupportVectorRegression, "Kaggle_LinearSupportVectorRegression")
+    ip.customPickle(word_features, "KaggleN5000_word_features")
 
     print('Finished :{}'.format(str(datetime.datetime.now()-start)))
+
+
 
 train_on_kaggle_data()
 
