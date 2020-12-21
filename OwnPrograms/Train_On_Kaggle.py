d84d0156f6e3acb535cf82de06c91af09151822a
Parker Burchett <parkerburchett@gmail.com>
2020-12-21 15:31:16 -0800

    Added Time Testing and log files

--- OwnPrograms/Train_On_Kaggle.py
+++ OwnPrograms/Train_On_Kaggle.py
@@ -13,16 +13,26 @@ def convert_docs_to_vectors(docs, word_features):
                 word_features: the list of word_features to be treated as dems to classify the things
 
     Returns:
-        Vectors: An np.Array of Boolean Vectors of Llngth =len(word_features).Represents each of the points in the dataset
-            A tweet is mapped on to a boolean vector by looking at
-        Targets: The associated Positive or Negative sentiment to with that vector
+        Vectors: An np.array of Boolean Vectors of length =len(word_features).
+        It represents each of the tweets in the dataset
+            A tweet is mapped on to a boolean vector by looking at what words are contained in it.
 
+            For example, if the "great" is in word_features at index 12.
+            Every tweet that contains "great" will have a True at index 12 and every tweet without "great" will have a false
+
+            The way I think about it is: each tweet is converted into a point in a num_features Dimensional space.
+            Then the algos now that they have the a collection of point -> output pairs do thier best to draw a hyperplane
+            A num_features -1 dimensional subspace such that the it divides the positive and negative tweets the best it can.
+        Targets: The associated Positive or Negative sentiment to with that Vector of the same index
     """
     num_features = len(word_features) # constant
     vectors = np.zeros((len(docs), num_features), dtype=bool)  # this is where you store the results. Default is false
     targets = [] # a list should work here it might be slower
     for doc in range(len(docs)):
         words = nltk.word_tokenize(docs[doc][0])
+        # at this point you would want to limit words by things like stop words, punctuation, or Parts of Speech
+        # make sure that those words are also excluded from word_features.
+        # you can also add in (if not already existing) some words you think might have a relationship with sentiment
         sentiment = str(docs[doc][1])
         vector = np.zeros(num_features, dtype=bool)
 
@@ -31,7 +41,10 @@ def convert_docs_to_vectors(docs, word_features):
                 vector[i] = True
 
         vectors[doc] = vector
-        targets.append(sentiment)
+        if(sentiment =='Negative'):
+            targets.append(0) #negative sentiment is a zero
+        else:
+            targets.append(1) # positive sentiment is a one
 
     return vectors, targets
 
@@ -40,15 +53,20 @@ def convert_docs_to_vectors(docs, word_features):
 
 
 
+# youtube lecture
+# Source: https://www.youtube.com/watch?v=KTeVOb8gaD4
+# Source: https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVR.html
 
-
-# source: https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVR.html
 def train_on_kaggle_data():
+    out = open('log_kaggle.txt','a')
     start = datetime.datetime.now()
-    print('started')
+    num_tweets =100000
+    num_features =5000
+    out.write('When num_tweets is {}\n'.format(num_tweets))
+    out.write('When num_features is {}\n'.format(num_features))
     inputFile = open(r"C:\Users\parke\Documents\GitHub\NaturalLanguage\NaturalLanguage\Datasets\LabeledTweets.csv", "r")
     documents = dl.assemble_kaggle_documents(inputFile) # this shuffles it
-    docs = documents[:10000] # for debugging only treat the first 10k as sample
+    docs = documents[:num_tweets] # for debugging only treat the first N as sample
     all_words = []
     for d in docs:
         words = nltk.word_tokenize(d[0])
@@ -56,61 +74,29 @@ def train_on_kaggle_data():
             all_words.append(w.lower())
 
     all_wordsFRQ = nltk.FreqDist(all_words)
-
-    word_features_as_tuples = all_wordsFRQ.most_common(50) # A much better version. Make sure you replace this in main. You don't need to do any sorti
+    word_features_as_tuples = all_wordsFRQ.most_common(num_features) # A much better version. Make sure you replace this in main. You don't need to do any sorti
     word_features=[]
     for w in word_features_as_tuples:
         word_features.append(w[0])
+    out.write('Created word_features :{}\n'.format(str(datetime.datetime.now() - start)))
 
-    print('Created word_features :{}'.format(str(datetime.datetime.now() - start)))
+
     vectors, targets = convert_docs_to_vectors(docs,word_features)
-    # while here you need to also create the targets
+    out.write('Created vectors and targets :{}\n'.format(str(datetime.datetime.now() - start)))
 
 
-    print('Created vectors and targets :{}'.format(str(datetime.datetime.now() - start)))
+    my_classifier = LinearSVR(max_iter=1000, dual=True) # this is 10x the iterations. Still does not converge
+    my_classifier.fit(vectors,targets) # right now this trains on the entire dataset
+    out.write('Trained LinearSVC:{}\n'.format(str(datetime.datetime.now() - start)))
 
 
-    for i in range(10):
-        print(vectors[i])
-        print(targets[i])
+    out.write('Finished :{}\n\n'.format(str(datetime.datetime.now()-start)))
+    out.close()
 
 
-
-
-
-
-
-
-    #
-    # # very High Time cost. You might just want to pickle this after it runs.
-    # feature_sets = [(dl.find_Features(text, word_features), category)
-    #                 for (text, category) in docs]
-    # random.shuffle(feature_sets)
-    # print('Created feature_sets :{}'.format(str(datetime.datetime.now() - start)))
-    #
-    # train_set = feature_sets[:9000]
-    # test_set = feature_sets[9000:]
-    #
-    # res = convert_data_to_array(feature_sets)
-    #
-    #
-    # my_classifier = LinearSVR()
-    #
-    #
-    #
-    #
-    # data =[]
-    # targets =[]
-
-
-
-    # my_classifier.fit(vectors,cats)
-    # you need to convert vectors to a 2d boolean array
-    # you need to convert cats to a 1d string array
-
-
-    print('Finished :{}'.format(str(datetime.datetime.now()-start)))
+    #https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
+    #Use this to write an accuracy method.
 
 train_on_kaggle_data()
 
-# when
+
