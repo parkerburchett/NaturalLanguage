eeea1129b24ba2529c31ddf649e101021f504a62
Parker Burchett <parkerburchett@gmail.com>
2020-12-21 14:33:17 -0800

    Added method to convert to vectors and targets

--- OwnPrograms/Train_On_Kaggle.py
+++ OwnPrograms/Train_On_Kaggle.py
@@ -5,7 +5,41 @@ from NaturalLanguage.custom_NLTK_Utils import Inital_Pickle as ip
 import nltk
 import datetime
 import random
-import numpy
+import numpy as np
+
+def convert_docs_to_vectors(docs, word_features):
+    """
+    Parameters: docs a list of (dict, category) tuples that represent a point, output pair.
+                word_features: the list of word_features to be treated as dems to classify the things
+
+    Returns:
+        Vectors: An np.Array of Boolean Vectors of Llngth =len(word_features).Represents each of the points in the dataset
+            A tweet is mapped on to a boolean vector by looking at
+        Targets: The associated Positive or Negative sentiment to with that vector
+
+    """
+    num_features = len(word_features) # constant
+    vectors = np.zeros((len(docs), num_features), dtype=bool)  # this is where you store the results. Default is false
+    targets = [] # a list should work here it might be slower
+    for doc in range(len(docs)):
+        words = nltk.word_tokenize(docs[doc][0])
+        sentiment = str(docs[doc][1])
+        vector = np.zeros(num_features, dtype=bool)
+
+        for i in range(num_features):
+            if word_features[i] in words:
+                vector[i] = True
+
+        vectors[doc] = vector
+        targets.append(sentiment)
+
+    return vectors, targets
+
+
+
+
+
+
 
 
 # source: https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVR.html
@@ -13,14 +47,8 @@ def train_on_kaggle_data():
     start = datetime.datetime.now()
     print('started')
     inputFile = open(r"C:\Users\parke\Documents\GitHub\NaturalLanguage\NaturalLanguage\Datasets\LabeledTweets.csv", "r")
-    documents = dl.assemble_kaggle_documents(inputFile)
+    documents = dl.assemble_kaggle_documents(inputFile) # this shuffles it
     docs = documents[:10000] # for debugging only treat the first 10k as sample
-    # 10k = 14 seconds
-    # 20k = 28 seconds.
-    # 30k = 41 seconds
-    # when len(word_features) == 1000
-
-    # Time cost is linear with this equation: Total time  = (
     all_words = []
     for d in docs:
         words = nltk.word_tokenize(d[0])
@@ -29,45 +57,59 @@ def train_on_kaggle_data():
 
     all_wordsFRQ = nltk.FreqDist(all_words)
 
-    word_features_as_tuples = all_wordsFRQ.most_common(10) # A much better version. Make sure you replace this in main. You don't need to do any sorti
+    word_features_as_tuples = all_wordsFRQ.most_common(50) # A much better version. Make sure you replace this in main. You don't need to do any sorti
     word_features=[]
     for w in word_features_as_tuples:
         word_features.append(w[0])
 
     print('Created word_features :{}'.format(str(datetime.datetime.now() - start)))
-
-    # very High Time cost. You might just want to pickle this after it runs.
-    feature_sets = [(dl.find_Features(text, word_features), category)
-                    for (text, category) in docs]
-    random.shuffle(feature_sets)
-    train_set = feature_sets[:9000]
-    test_set = feature_sets[9000:]
-
-    my_classifier = LinearSVR()
+    vectors, targets = convert_docs_to_vectors(docs,word_features)
+    # while here you need to also create the targets
 
 
-    myData = numpy.array(feature_sets)
-    vectors =[]
-    cats =[]
+    print('Created vectors and targets :{}'.format(str(datetime.datetime.now() - start)))
 
-    for d in myData:
-        vectors.append(list((d[0].items())))
-        cats.append(d[1])
 
-    my_classifier.fit(vectors,cats)
+    for i in range(10):
+        print(vectors[i])
+        print(targets[i])
+
+
+
+
+
+
+
+
+    #
+    # # very High Time cost. You might just want to pickle this after it runs.
+    # feature_sets = [(dl.find_Features(text, word_features), category)
+    #                 for (text, category) in docs]
+    # random.shuffle(feature_sets)
+    # print('Created feature_sets :{}'.format(str(datetime.datetime.now() - start)))
+    #
+    # train_set = feature_sets[:9000]
+    # test_set = feature_sets[9000:]
+    #
+    # res = convert_data_to_array(feature_sets)
+    #
+    #
+    # my_classifier = LinearSVR()
+    #
+    #
+    #
+    #
+    # data =[]
+    # targets =[]
+
+
+
+    # my_classifier.fit(vectors,cats)
+    # you need to convert vectors to a 2d boolean array
+    # you need to convert cats to a 1d string array
 
 
     print('Finished :{}'.format(str(datetime.datetime.now()-start)))
-    # this is the syntax for fiting the algo: fit(Array maybe of vectors, Array maybe of proper outcomes.
-
-    # you could convert the list of (dict, String) tuples into a dataset and
-
-    # print('Accuracy: {}'.format(str(accuracy)))
-    # ip.customPickle(LinearSupportVectorRegression, "Kaggle_LinearSupportVectorRegression")
-    # ip.customPickle(word_features, "KaggleN5000_word_features")
-
-
-
 
 train_on_kaggle_data()
 
