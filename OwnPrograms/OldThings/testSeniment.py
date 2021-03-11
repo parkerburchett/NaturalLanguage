35a8231ca7a1c67dcc725c34e47b5e4fefa5a2bc
Parker Burchett <57877709+parkerburchett@users.noreply.github.com>
2021-03-11 13:35:52 -0800

    fixed typos

--- OwnPrograms/OldThings/testSeniment.py
+++ OwnPrograms/OldThings/testSeniment.py
@@ -7,8 +7,6 @@ print(s.sentiment("I found the movie trite and plot development lacking"))
 
 print(s.sentiment("The cinematography was breathtaking in its ability to capture the mood of the era"))
 
-print(s.sentiment("it's too bad my schlong is this long otheriwise i'd be banging bitch's all day long"))
-
 print(s.sentiment("it made me laugh"))
 
 print(s.sentiment("The flavor of this film was terrible and I kicked the bucket after I watched this film, great acting. Pasta"))
